617b7594a2e7184d981d533f2a5bad2fd90fba10
BAD BABY <154504921+Badhacker98@users.noreply.github.com>
2024-01-09 19:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MXNIHACKER/BrokenMusic",
+    "https://github.com/adityatel/VIVEK-kUMAR",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
